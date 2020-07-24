585afc1220d0b14e3e814824dd7433387b0d234e
AIT-LAHCEN <ah.aitlahcen@gmail.com>
2020-07-24 13:02:01 +0100

    navbar v 1.1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Button,Nav,Navbar,Form,FormControl } from 'react-bootstrap';
 
 function App(){
   return(
-    <div>
+    <div class="header-bar clearfix">
         <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Orbis Agricole</Navbar.Brand>
         <Nav className="mr-auto">
@@ -16,9 +16,13 @@ function App(){
           <Nav.Link href="#gestion">Gestion et Droit</Nav.Link>
         </Nav>
         <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
+          <FormControl type="text" placeholder="Recherche" className="mr-sm-2" />
         </Form>
+        <ul class="nav navbar-nav navbar-right">
+          
+          <li><a href="#signup"><Button variant="outline-info"> S'abonner</Button></a></li>
+          <li><a href="#login"><Button variant="outline-info"> Connexion</Button></a></li>
+        </ul>
       </Navbar>
     </div>
   );
