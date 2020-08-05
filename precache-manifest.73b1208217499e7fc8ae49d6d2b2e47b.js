d540d1472aa7cd49a75ed687a8d2f6263d4de950
AIT-LAHCEN <ah.aitlahcen@gmail.com>
2020-08-05 20:36:45 +0100

    Updates

---
+++ precache-manifest.73b1208217499e7fc8ae49d6d2b2e47b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b4ae798e7479dc38809c495c970db",
+    "url": "/orbis-agricole/index.html"
+  },
+  {
+    "revision": "2f664ef48a33e2061b5f",
+    "url": "/orbis-agricole/static/css/2.8a5276d1.chunk.css"
+  },
+  {
+    "revision": "fab1bd3a663dae5e3e9e",
+    "url": "/orbis-agricole/static/css/main.21e02799.chunk.css"
+  },
+  {
+    "revision": "2f664ef48a33e2061b5f",
+    "url": "/orbis-agricole/static/js/2.9c0ed106.chunk.js"
+  },
+  {
+    "revision": "38a4b558739197dd9ec900c0561b7e3e",
+    "url": "/orbis-agricole/static/js/2.9c0ed106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab1bd3a663dae5e3e9e",
+    "url": "/orbis-agricole/static/js/main.c36a5641.chunk.js"
+  },
+  {
+    "revision": "0e32f684c4d4ea7083e2",
+    "url": "/orbis-agricole/static/js/runtime-main.778363fd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/orbis-agricole/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/orbis-agricole/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/orbis-agricole/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/orbis-agricole/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/orbis-agricole/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
