df7a70f23b756a0682183720a35a14e5f2714d9d
AIT-LAHCEN <ah.aitlahcen@gmail.com>
2020-08-08 20:11:21 +0100

    react redux form validation

--- src/Components/Inscription.js
+++ src/Components/Inscription.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { Button, Label, Col, Row } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+const validPassword = (val) => new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})").test(val);
+
 class Inscription extends Component {
 
     constructor(props) {
@@ -30,6 +37,19 @@ class Inscription extends Component {
                                         <Control.text model=".firstname" id="firstname" name="firstname"
                                             placeholder="Entrer votre nom"
                                             className="form-control"
+                                            validators={{
+                                                required, minLength: minLength(3), maxLength: maxLength(20)
+                                            }}
+                                            />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".firstname"
+                                                show="touched"
+                                                messages={{
+                                                    required: 'Obligatoire ',
+                                                    minLength: 'Doit contenir plus de 2 caractères',
+                                                    maxLength: 'Ne dépassez pas 20 caractères'
+                                                }}
                                             />
                                     </Col>
                                 </Row>
@@ -39,6 +59,19 @@ class Inscription extends Component {
                                         <Control.text model=".lastname" id="lastname" name="lastname"
                                             placeholder="Entrer votre prénom"
                                             className="form-control"
+                                            validators={{
+                                                required, minLength: minLength(3), maxLength: maxLength(20)
+                                            }}
+                                            />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".lastname"
+                                                show="touched"
+                                                messages={{
+                                                    required: 'Obligatoire ',
+                                                    minLength: 'Doit contenir plus de 2 caractères',
+                                                    maxLength: 'Ne dépassez pas 20 caractères'
+                                                }}
                                             />
                                     </Col>                        
                                 </Row>
@@ -48,6 +81,20 @@ class Inscription extends Component {
                                         <Control.text model=".telnum" id="telnum" name="telnum"
                                             placeholder="Entrer votre numéro de mobile"
                                             className="form-control"
+                                            validators={{
+                                                required, minLength: minLength(2), maxLength: maxLength(13), isNumber
+                                            }}
+                                            />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".telnum"
+                                                show="touched"
+                                                messages={{
+                                                    required: 'Obligatoire ',
+                                                    minLength: "Doit contenir plus d'un numéro ",
+                                                    maxLength: 'Ne dépassez pas 13 numéros ',
+                                                    isNumber: 'Doit être un nombre'
+                                                }}
                                             />
                                     </Col>
                                 </Row>
@@ -57,6 +104,18 @@ class Inscription extends Component {
                                         <Control.text model=".email" id="email" name="email"
                                             placeholder="Entrer votre adresse e-mail"
                                             className="form-control"
+                                            validators={{
+                                                required, validEmail
+                                            }}
+                                            />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".email"
+                                                show="touched"
+                                                messages={{
+                                                    required: 'Obligatoire ',
+                                                    validEmail: 'Adresse e-mail invalide'
+                                                }}
                                             />
                                     </Col>
                                 </Row>
@@ -83,18 +142,41 @@ class Inscription extends Component {
                                 <Row className="form-group">
                                     <Label htmlFor="password" md={4}>Mot de passe</Label>
                                     <Col md={8}>
-                                        <Control.text model=".password" id="password" name="password"
+                                        <Control.password model=".password" id="password" name="password"
                                             placeholder="Entrer votre mot de passe"
                                             className="form-control"
+                                            validators={{
+                                                required, validPassword
+                                            }}
+                                            />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".password"
+                                                show="touched"
+                                                messages={{
+                                                    required: 'Obligatoire ',
+                                                    validPassword: 'Le mot de passe doit contenir au moins un caractère majuscule, minuscule, numéro et un caractère spécial au total 8 caractères ou plus'
+                                                }}
                                             />
                                     </Col>
                                 </Row>
                                 <Row className="form-group">
-                                    <Label htmlFor="password" md={4}>Confirmation de mot de passe</Label>
+                                    <Label htmlFor="repassword" md={4}>Confirmation de mot de passe</Label>
                                     <Col md={8}>
-                                        <Control.text model=".password" id="password" name="password"
+                                        <Control.password model=".repassword" id="repassword" name="repassword"
                                             placeholder="Retaper le mot de passe"
                                             className="form-control"
+                                            validators={{
+                                                required
+                                            }}
+                                            />
+                                            <Errors
+                                                className="text-danger"
+                                                model=".password"
+                                                show="touched"
+                                                messages={{
+                                                    required: 'Obligatoire',
+                                                }}
                                             />
                                     </Col>
                                 </Row>
