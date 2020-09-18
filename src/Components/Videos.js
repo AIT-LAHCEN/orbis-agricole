3bb245bf9cbe71457e67759bb4865b28ce961e28
AIT-LAHCEN <ah.aitlahcen@gmail.com>
2020-09-18 18:34:40 +0100

    adding a video background

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import backvid from '../videos/the-farmer_Rlqc19AY_MMD1.mp4';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpa } from '@fortawesome/free-solid-svg-icons'
 
 class Videos extends Component{
     render(){
         return(
-            //React.Fragment au lieu de div 
             <div className="videos">
-                <h2>A props de Orbis Agro Industry</h2>
-                <h4>
-                Orbis Agro Industry – Animal est une filiale d’Orbis Holding. Elle regroupe l’ensemble des activités liées à l’élevage, au grossissement et à la production laitière du Groupe; avec une production annuelle de 14 millions de litres de lait par an.
-Avec les objectifs stratégiques de renforcer la production laitière via d’autres fermes laitières et de développer une unité de transformtion du lait, Orbis Agro Industry – Animal croitra ses chaînes de production et se positionnera en tant qu’industriel laitier.
-                </h4>
-                <h4>Au Maroc, le lait est une boisson très prisée par les ménages et reste un produit incontournable des tables marocaines. Il se décline sous plusieurs formes dans les épiceries et au sein des grandes et moyennes surfaces (lait frais, yaourts, crèmes, etc.).
-De ce fait, le marché du lait au Maroc connait une très forte dynamique, dopée par une évolution de son mode de consommation et une production nationale en deça des capacités du marché.</h4>
-
-                <h4>Orbis Agro Industry – Animal a pour but de produire un lait d’excellente qualité et de garder une place prépondérante parmi les meilleurs producteurs marocains et ce, dans un contexte national très concurrentiel.</h4>
+                <div className="col-12 padd">
+                        <h3><FontAwesomeIcon icon={faSpa} /> A props de Orbis Agro Industry</h3>
+                        <hr/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        
+                </div>
+                <img className="col-2 float-left" src="orbis-agricole/images/orb1.jpg" alt="orbis agro industry animal"/>
+                <img className="col-2 float-right" src="orbis-agricole/images/orb2.jpg" alt="orbis agro industry vigital"/>
+                <video autoPlay loop muted
+                style={{
+                    position: "relative",
+                    width: "100%",
+                    left: "50%",
+                    top: "100%",
+                    height: "85%",
+                    objectFit: "cover",
+                    transform: "translate(-50%, -82%)",
+                    zIndex: "-1"
+                }}>
+                    <source src={backvid} type="video/mp4"/>
+                </video>
+                
             </div>
         );
     }
